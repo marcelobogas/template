e16511b14303320932a35b4b1c82983091569bb6
Marcelo Bogás <marcelobogas@hotmail.com>
2021-11-03 14:50:31 -0300

    change index.php file

--- index.php
+++ index.php
@@ -4,35 +4,27 @@ use App\Core\Enviroments;
 
 require __DIR__ . '/vendor/autoload.php';
 
-/* carrega as variáveis de ambiente */
+/* carrega as variáveis de ambiente para o projeto */
 Enviroments::load(__DIR__ . '/config');
 
-/* inclui a página inicial */
-$url = (isset($_GET['url'])) ? $_GET['url'] : 'home';
-
-/* define o diretório para verificação de url */
-$diretorio = 'pages';
-
-/* definição de páginas permitidas */
-$paginasPermitidas = array('404', 'home', 'about', 'blog', 'contact', 'courses');
-
-if (substr_count($url, '/') > 0) {
-
-    /* converte a url em array */
-    $url = explode('/', $url);
-
+if (!isset($_GET['url'])) {
     /* define título da página */
-    define('APP_TITLE', ucfirst($url[0]));
+    define('APP_TITLE', ucfirst('home'));
 
-    /* verifica se a página requisitada existe */
-    $pg = (file_exists("{$diretorio}/" . $url[0] . '.php')) && in_array($url[0], $paginasPermitidas) ? $url[0] : '404';
-} else {
-
-    /* define título da página */
-    define('APP_TITLE', ucfirst($url));
-
-    $pg = (file_exists("{$diretorio}/" . $url . '.php')) && in_array($url, $paginasPermitidas) ? $url : '404';
+    /* redireciona para a págian inicial */
+    require(__DIR__ . '/pages/home.php');
+    exit;
 }
 
-/* adiciona a página */
-require("{$diretorio}/{$pg}.php");
+/* converte a url em array */
+$url = explode('/', $_GET['url']);
+
+/* verifica se o arquivo existe para adicionar a página */
+$pg = (file_exists(__DIR__ . '/pages//' . $url[0] . '.php') ? $url[0] : '404');
+
+/* define título da página */
+define('APP_TITLE', ucfirst(str_replace('-', ' ', $pg)));
+
+/* adiciona a página solicitada */
+require(__DIR__ . '/pages//' . $pg . '.php');
+exit;
